5134ee5f630ae72a8ec7c77167f25f40d50d718f
Johndel Torrizo <johndeltorrizo@gmail.com>
2023-02-06 06:49:10 +0000

    Personal Infos

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Johndel Patrick Torrizo",
+  title: "Mechatronics Engineering Student & Aspiring Software Dev",
+  email: "johndeltorrizo@gmail.com",
+  gitHub: "https://github.com/JohndelTorrizo",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
